c0d2ada59f6379b6cfd9014c4e66ad877cd4aebf
mtoto <tszilagyi@outlook.com>
2019-11-24 23:19:05 +0100

    git line fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -45,8 +45,8 @@ sh -c "aws s3 sync s3://${AWS_S3_BUCKET}/${DEST_FILE} ${DEST_DIR}\${DEST_FILE} \
               ${ENDPOINT_APPEND} $*"
 
 # Commit changes to github
-git commit ${DEST_DIR}\${DEST_FILE} -m 'Commit fresh fight data';
-git push https://${{github.actor}}:${{secrets.GITHUB_TOKEN}}@github.com/${{github.repository}}.git HEAD:master
+sh -c "git commit ${DEST_DIR}\${DEST_FILE} -m 'Commit fresh fight data'"
+sh -c "git push https://${{github.actor}}:${{secrets.GITHUB_TOKEN}}@github.com/${{github.repository}}.git HEAD:master"
 
 # Clear out credentials after we're done.
 # We need to re-run `aws configure` with bogus input instead of
